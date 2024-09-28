00b64256fa761483ab036d727472147d7a0dcfab
Mehedi <me69oshan@gmail.com>
2024-09-29 04:20:14 +0600

    dom observer based solution

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,49 +4,66 @@ const String ytWebRoute = "/yt-web";
 
 // JS injection strings
 const String ytShortsBlockedJS = '''
-              // Block/remove Shorts related elements
+  // Block/remove Shorts page
+  function removeShorts() {
+    console.log('Checking for Shorts elements');
+    var isShortsPage = window.location.href.includes('shorts');
+    if (isShortsPage) {
+      var shortsPage = document.querySelector("shorts-video");
+      if (shortsPage) {
+        shortsPage.remove();
+        console.log('Shorts page removed');
+      }
+    }
+  }
 
-              setInterval(function() {
-                var isShortsPage = window.location.href.includes('shorts');
-                if (isShortsPage) {
-                  document.querySelector("shorts-page").remove();
-                }
-              }, 5000);
-            ''';
+  // Initial removal
+  removeShorts();
+
+  // Set up a MutationObserver to watch for changes in the DOM
+  var observer = new MutationObserver(function(mutations) {
+    removeShorts();
+  });
+
+  // Start observing the document with the configured parameters
+  observer.observe(document.body, { childList: true, subtree: true });
+''';
 const String ytNoShortsJS = '''
-              // Block or hide all Shorts elements
+  // Block/remove Shorts related elements
+  function removeShorts() {
+    console.log('Checking for Shorts elements');
+    var isShortsPage = window.location.href.includes('shorts');
+    if (isShortsPage) {
+      var shortsPage = document.querySelector("shorts-video");
+      if (shortsPage) {
+        shortsPage.remove();
+        console.log('Shorts page removed');
+      }
+    }
 
-              var shortsTab = document.querySelectorAll("ytm-pivot-bar-item-renderer")[1];
-              if (shortsTab.innerText === "Shorts") {
-                shortsTab.remove();
-              }
-                
-              setInterval(function() {
-                var isShortsPage = window.location.href.includes('shorts');
-                if (isShortsPage) {
-                  document.querySelector("shorts-page").remove();
-                }
-              }, 10000);
+    var shortsTab = document.querySelectorAll("ytm-pivot-bar-item-renderer")[1];
+    if (shortsTab && shortsTab.innerText === "Shorts") {
+      shortsTab.remove();
+      console.log('Shorts tab removed');
+    }
 
-              setInterval(function() {
-                var shortsSections = document.querySelectorAll("ytm-reel-shelf-renderer");
-                if (shortsSections) {
-                  shortsSections.forEach((shortsSection) => {
-                    if(shortsSection.innerText.includes("Shorts")){
-                      shortsSection.remove();
-                    }
-                  });
-                }
-              }, 5000);
+    var shortsSections = document.querySelectorAll("ytm-reel-shelf-renderer, ytm-rich-section-renderer");
+    shortsSections.forEach((shortsSection) => {
+      if(shortsSection.innerText.includes("Shorts")){
+        shortsSection.remove();
+        console.log('Shorts section removed');
+      }
+    });
+  }
 
-              setInterval(function() {
-                var shortsSections = document.querySelectorAll("ytm-rich-section-renderer");
-                if (shortsSections) {
-                  shortsSections.forEach((shortsSection) => {
-                    if(shortsSection.innerText.includes("Shorts")){
-                      shortsSection.remove();
-                    }
-                  });
-                }
-              }, 5000);
-            ''';
+  // Initial removal
+  removeShorts();
+
+  // Set up a MutationObserver to watch for changes in the DOM
+  var observer = new MutationObserver(function(mutations) {
+    removeShorts();
+  });
+
+  // Start observing the document with the configured parameters
+  observer.observe(document.body, { childList: true, subtree: true });
+''';
