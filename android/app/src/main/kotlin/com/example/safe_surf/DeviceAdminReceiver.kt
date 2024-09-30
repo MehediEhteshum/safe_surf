4f76743f88d1f150e9f59352a5d7c9a22bbc45ee
Mehedi <me69oshan@gmail.com>
2024-10-01 04:29:36 +0600

    test only prop added for device owner app test; uninstall from app info also lock prevented

--- android/app/src/main/kotlin/com/example/safe_surf/DeviceAdminReceiver.kt
+++ android/app/src/main/kotlin/com/example/safe_surf/DeviceAdminReceiver.kt
@@ -6,11 +6,18 @@ import android.content.Context
 import android.content.Intent
 
 class MyDeviceAdminReceiver : DeviceAdminReceiver() {
+
     override fun onDisableRequested(context: Context, intent: Intent): CharSequence {
         // Lock the screen
         val devicePolicyManager = context.getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
         devicePolicyManager.lockNow()
         
         return "Disabling device admin will remove security features of this app."
+    }
+
+    override fun onDisabled(context: Context, intent: Intent) {
+        // Lock the screen
+        val devicePolicyManager = context.getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        devicePolicyManager.lockNow()
     }
 }
